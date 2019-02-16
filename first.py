d42e183b128f8854f02b904d2e6f44f243b56964
Pravin Fonseka <pf984@nyu.edu>
2019-02-15 22:15:18 -0500

    first commit

---
+++ first.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    # return "Hello, World!"
+    print(request.get_json())
+    jsondata = request.get_json()
+    print(jsondata['test1'])
+    return "hi"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
